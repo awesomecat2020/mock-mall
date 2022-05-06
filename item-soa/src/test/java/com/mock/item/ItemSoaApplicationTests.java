005a101e5166a4c005bf12c876a23fe925969c48
awesomecat <782453374@qq.com>
2022-05-06 18:55:14 +0800

    init order/item

---
+++ item-soa/src/test/java/com/mock/item/ItemSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemSoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ order-soa/src/test/java/com/mock/order/OrderSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
