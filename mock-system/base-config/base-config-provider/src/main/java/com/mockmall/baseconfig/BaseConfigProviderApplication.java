defe1820bf338d99b08a9215fc261c6ff7c1afbe
ccomma <782453374@qq.com>
2024-12-14 19:21:50 +0800

    feat(base-config): Add Basic Configuration Module

---
+++ mock-system/base-config/base-config-provider/src/main/java/com/mockmall/baseconfig/BaseConfigProviderApplication.java
@@ -0,0 +1,13 @@
+package com.mockmall.baseconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseConfigProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseConfigProviderApplication.class, args);
+	}
+
+}
